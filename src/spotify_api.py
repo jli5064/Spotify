e3e2ca3a92263c9ac9503c254ed34d539dc967fb
darehunt <42155557+darehunt@users.noreply.github.com>
2023-02-14 18:55:57 -0800

    changed api .py file

--- src/spotify_api.py
+++ src/spotify_api.py
@@ -1,48 +1,20 @@
-import requests
-import json
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
-def get_access_token():
-    auth_url = 'https://accounts.spotify.com/api/token'
-    data = {
-    'grant_type': 'client_credentials',
-    'client_id': "8ac5bd4f29854a4abb37648a5b7833d8",
-    'client_secret': "c9f1281a0f774d97ae7d002139e53049",
-    }
-    auth_response = requests.post(auth_url, data=data)
-    access_token = auth_response.json().get('access_token')
-    ACCESS_TOKEN_LINE = "Bearer " + access_token
-    return ACCESS_TOKEN_LINE
+def get_artist_genres(artist_names):
+    client_id = '8ac5bd4f29854a4abb37648a5b7833d8'
+    client_secret = 'c9f1281a0f774d97ae7d002139e53049'
+    client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-def get_artist_genres(access, artist_names):
-    # Set up the API endpoint and headers
-    endpoint = "https://api.spotify.com/v1/search"
-    headers = {
-        "Authorization": access
-    }
-
-    # Create an empty list to store the genres
-    artist_genres = {}
-    i = 0
-    # Iterate over the list of artists
-    for artist_name in artist_names:
-        # Specify the search parameters
-        params = {
-            "q": artist_name,
-            "type": "artist"
-        }
-        # print(params)
-        # Send a GET request to the API endpoint
-        response = requests.get(endpoint, headers=headers, params=params)
-        i += 1
-        print(response, i)
-        # Parse the response as JSON
-        data = json.loads(response.text)
-        # Append the artist's genres to the list of genres
-        result = data["artists"]["items"]
+    artist_dict = {}
+    
+    for artist in artists:
+        result = sp.search(artist, type='artist')['artists']['items']
         if len(result) > 0:
-            artist_genres[artist_name] = result[0]['genres']
-    return artist_genres
+            artist_dict[artist] = result[0]['genres']
+    return artist_dict
 
-def get_spotify_genres(access_token, artists):
-    genres = get_artist_genres(access_token, artists)
-    return genres
+# def get_spotify_genres(access_token, artists):
+#     genres = get_artist_genres(access_token, artists)
+#     return genres
