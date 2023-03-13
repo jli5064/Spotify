b728eee731d87119709778483e82ab255e736976
darehunt <42155557+darehunt@users.noreply.github.com>
2023-03-12 23:11:38 -0700

    don't think we need test-data instance

--- run.py
+++ run.py
@@ -26,6 +26,7 @@ def main(targets):
     model_test = False
 
     make_data_dir()
+    print(df)
 
     if 'clean' in targets: # works
         clean()
@@ -49,7 +50,7 @@ def main(targets):
         if df is not None:
             print("saving sampled data as own file")
             # sample size of 2000 playlists
-            df = create_test_sample(2000, df, kaggle_config["data_dir"], kaggle_config["data_filename"])
+            df = create_test_sample(2000, df, kaggle_config["data_dir"], kaggle_config["raw_data_filename"])
             print("test df created!")
         else:
             print("sampled data already exists")
@@ -58,25 +59,25 @@ def main(targets):
         cleaned_G = kaggle_clean_graph_edges(G)
         dump_graph(cleaned_G, os.path.join(kaggle_config["temp_dir"], kaggle_config[ "pickle_graph_filename"]))
 
-    if ('test-data' in targets):
-        print("This will download kaggle spotify data (test)")
-        model_test = True
+    # if ('test-data' in targets):
+    #     print("This will download kaggle spotify data (test)")
+    #     model_test = True
 
-        with open('config/kaggle.json') as fh:
-            kaggle_config = json.load(fh)
-        pull_kaggle_data()
-        df = read_in_raw_csv()
-        if df is not None:
-            print("saving sampled data as own file")
-            df = create_test_sample(df, kaggle_config["test_data_dir"], kaggle_config["test_data_filename"])
-            print("test df created!")
-        else:
-            print("sampled data already exists")
-        G = kaggle_generate_graph(df, os.path.join(kaggle_config["test_temp_dir"], kaggle_config[ "group_df_filename"]))
-        print("graph properly generated")
-        cleaned_G = kaggle_clean_graph_edges(G)
-        print("graph edges trimmed")
-        dump_graph(cleaned_G, os.path.join(kaggle_config["test_temp_dir"], kaggle_config[ "test_pickle_graph_filename"]))
+    #     with open('config/kaggle.json') as fh:
+    #         kaggle_config = json.load(fh)
+    #     pull_kaggle_data()
+    #     df = read_in_raw_csv()
+    #     if df is not None:
+    #         print("saving sampled data as own file")
+    #         df = create_test_sample(df, kaggle_config["test_data_dir"], kaggle_config["test_data_filename"])
+    #         print("test df created!")
+    #     else:
+    #         print("sampled data already exists")
+    #     G = kaggle_generate_graph(df, os.path.join(kaggle_config["test_temp_dir"], kaggle_config[ "group_df_filename"]))
+    #     print("graph properly generated")
+    #     cleaned_G = kaggle_clean_graph_edges(G)
+    #     print("graph edges trimmed")
+    #     dump_graph(cleaned_G, os.path.join(kaggle_config["test_temp_dir"], kaggle_config[ "test_pickle_graph_filename"]))
 
     if 'model' in targets:
         if model_test:
@@ -88,7 +89,7 @@ def main(targets):
 
         # to do:
         # - run G through train()
-        #     - need to update bigclam to make attributes
+        #     - need to update bigclam.py to make attributes
         # - evaluate results
 
     #     print("running model, calculating accuracy")
